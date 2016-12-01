251cfafc8cf8e9c2c1d6fab2c7fd2af112bb1c0e
Gabriele Sammartino <Gabriele Sammartino>
2016-12-01 20:03:48 +0100

    Test1.1

--- routes/users.js
+++ routes/users.js
@@ -28,10 +28,10 @@ router.post('/',middle, function (req, res, next) {
   newUser.save(function(err){
     res.status(201).json(newUser);
   })
-});
+}); { $or: [{a: 1}, {b: 1}] }
 router.put('/:id',middle,function(req,res,next){
   if (req.params.id == 'all'){
-      var conditions = {}, update =( {$set: {name : req.body.name,surname : req.body.surname,age: req.body.age}}), options = { multi: true };
+      var conditions = {}, update =( {$set: {$or: [{name : req.body.name},{name : name}]},surname : req.body.surname ,age: req.body.age}), options = { multi: true };
       return User.update(conditions, update, options, callback);
       function callback (err, numAffected) {
         if(err) return res.status(500).json({message: 'Errore'});
